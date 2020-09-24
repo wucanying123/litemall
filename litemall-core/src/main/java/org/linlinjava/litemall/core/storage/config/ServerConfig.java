5faee9f0e93eb1af503c02ee8b64b9fbcb0a5081
DESKTOP-57TDA82\Admin <962933013@qq.com>
2020-09-24 08:52:07 +0800

    成功播放-局域网地址原因

--- litemall-core/src/main/java/org/linlinjava/litemall/core/storage/config/ServerConfig.java
+++ litemall-core/src/main/java/org/linlinjava/litemall/core/storage/config/ServerConfig.java
@@ -4,13 +4,23 @@ import org.springframework.boot.web.context.WebServerInitializedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @Component
 public class ServerConfig implements ApplicationListener<WebServerInitializedEvent> {
 
     private int serverPort;
 
     public String getUrl() {
-        return "http://localhost:"+this.serverPort;
+        InetAddress address = null;
+        try {
+            address = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "http://"+address.getHostAddress() +":"+this.serverPort;
+
     }
 
     @Override
