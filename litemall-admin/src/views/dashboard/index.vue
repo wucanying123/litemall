95b1f3019e4aae1edff3799539bb88fd869710e3
wucanying <962933013@qq.com>
2020-10-25 16:28:22 +0800

    隐藏别人的菜单

--- litemall-admin/src/views/dashboard/index.vue
+++ litemall-admin/src/views/dashboard/index.vue
@@ -1,62 +1,62 @@
 <template>
   <div class="dashboard-editor-container">
 
-    <el-row :gutter="40" class="panel-group">
-      <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-        <div class="card-panel" @click="handleSetLineChartData('newVisitis')">
-          <div class="card-panel-icon-wrapper icon-people">
-            <svg-icon icon-class="peoples" class-name="card-panel-icon" />
-          </div>
-          <div class="card-panel-description">
-            <div class="card-panel-text">用户数量</div>
-            <count-to :start-val="0" :end-val="userTotal" :duration="2600" class="card-panel-num"/>
-          </div>
-        </div>
-      </el-col>
-      <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-        <div class="card-panel" @click="handleSetLineChartData('messages')">
-          <div class="card-panel-icon-wrapper icon-message">
-            <svg-icon icon-class="message" class-name="card-panel-icon" />
-          </div>
-          <div class="card-panel-description">
-            <div class="card-panel-text">商品数量</div>
-            <count-to :start-val="0" :end-val="goodsTotal" :duration="3000" class="card-panel-num"/>
-          </div>
-        </div>
-      </el-col>
-      <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-        <div class="card-panel" @click="handleSetLineChartData('purchases')">
-          <div class="card-panel-icon-wrapper icon-money">
-            <svg-icon icon-class="message" class-name="card-panel-icon" />
-          </div>
-          <div class="card-panel-description">
-            <div class="card-panel-text">货品数量</div>
-            <count-to :start-val="0" :end-val="productTotal" :duration="3200" class="card-panel-num"/>
-          </div>
-        </div>
-      </el-col>
-      <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
-        <div class="card-panel" @click="handleSetLineChartData('shoppings')">
-          <div class="card-panel-icon-wrapper icon-shoppingCard">
-            <svg-icon icon-class="money" class-name="card-panel-icon" />
-          </div>
-          <div class="card-panel-description">
-            <div class="card-panel-text">订单数量</div>
-            <count-to :start-val="0" :end-val="orderTotal" :duration="3600" class="card-panel-num"/>
-          </div>
-        </div>
-      </el-col>
-    </el-row>
+    <!--    <el-row :gutter="40" class="panel-group">-->
+    <!--      <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">-->
+    <!--        <div class="card-panel" @click="handleSetLineChartData('newVisitis')">-->
+    <!--          <div class="card-panel-icon-wrapper icon-people">-->
+    <!--            <svg-icon icon-class="peoples" class-name="card-panel-icon" />-->
+    <!--          </div>-->
+    <!--          <div class="card-panel-description">-->
+    <!--            <div class="card-panel-text">用户数量</div>-->
+    <!--            <count-to :start-val="0" :end-val="userTotal" :duration="2600" class="card-panel-num"/>-->
+    <!--          </div>-->
+    <!--        </div>-->
+    <!--      </el-col>-->
+    <!--      <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">-->
+    <!--        <div class="card-panel" @click="handleSetLineChartData('messages')">-->
+    <!--          <div class="card-panel-icon-wrapper icon-message">-->
+    <!--            <svg-icon icon-class="message" class-name="card-panel-icon" />-->
+    <!--          </div>-->
+    <!--          <div class="card-panel-description">-->
+    <!--            <div class="card-panel-text">商品数量</div>-->
+    <!--            <count-to :start-val="0" :end-val="goodsTotal" :duration="3000" class="card-panel-num"/>-->
+    <!--          </div>-->
+    <!--        </div>-->
+    <!--      </el-col>-->
+    <!--      <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">-->
+    <!--        <div class="card-panel" @click="handleSetLineChartData('purchases')">-->
+    <!--          <div class="card-panel-icon-wrapper icon-money">-->
+    <!--            <svg-icon icon-class="message" class-name="card-panel-icon" />-->
+    <!--          </div>-->
+    <!--          <div class="card-panel-description">-->
+    <!--            <div class="card-panel-text">货品数量</div>-->
+    <!--            <count-to :start-val="0" :end-val="productTotal" :duration="3200" class="card-panel-num"/>-->
+    <!--          </div>-->
+    <!--        </div>-->
+    <!--      </el-col>-->
+    <!--      <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">-->
+    <!--        <div class="card-panel" @click="handleSetLineChartData('shoppings')">-->
+    <!--          <div class="card-panel-icon-wrapper icon-shoppingCard">-->
+    <!--            <svg-icon icon-class="money" class-name="card-panel-icon" />-->
+    <!--          </div>-->
+    <!--          <div class="card-panel-description">-->
+    <!--            <div class="card-panel-text">订单数量</div>-->
+    <!--            <count-to :start-val="0" :end-val="orderTotal" :duration="3600" class="card-panel-num"/>-->
+    <!--          </div>-->
+    <!--        </div>-->
+    <!--      </el-col>-->
+    <!--    </el-row>-->
   </div>
 </template>
 
 <script>
 import { info } from '@/api/dashboard'
-import CountTo from 'vue-count-to'
+// import CountTo from 'vue-count-to'
 
 export default {
   components: {
-    CountTo
+    // CountTo
   },
   data() {
     return {
