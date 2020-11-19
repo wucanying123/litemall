773529d25d66d5ad8d580c139e9944c484fc2229
962933013 <962933013@qq.com>
2020-11-19 10:18:01 +0800

    弄好样式

--- litemall-admin/src/views/screen/seniorProgramEdit.vue
+++ litemall-admin/src/views/screen/seniorProgramEdit.vue
@@ -1,7 +1,7 @@
 <template>
   <el-container style="border: 1px solid #eee">
     <el-aside
-      width="220px"
+      width="11%"
       style="background-color: rgb(238, 241, 246)"
     >
       <el-menu :default-openeds="['1','2','3','4','5']">
@@ -219,27 +219,38 @@
         status-icon
         label-position="left"
         label-width="100px"
-        style="width: 800px; margin-left:50px;"
+        style="width: 600px; margin-right: 5px"
       >
 
-        <el-form-item label="节目名称" prop="name"><el-input v-model="program.name" /></el-form-item>
+        <el-form-item label="节目名称" prop="name">
+          <el-input v-model="program.name" />
+        </el-form-item>
         <el-row>
           <el-col :span="12">
-            <el-form-item label="节目宽" prop="width"><el-input v-model="program.width" readonly />  </el-form-item>
+            <el-form-item label="节目宽" prop="width">
+              <el-input v-model="program.width" readonly />
+            </el-form-item>
           </el-col>
           <el-col :span="12">
-            <el-form-item label="节目高" prop="height"><el-input v-model="program.height" readonly />  </el-form-item>
+            <el-form-item label="节目高" prop="height">
+              <el-input v-model="program.height" readonly />
+            </el-form-item>
           </el-col>
         </el-row>
         <el-tabs>
           <el-tab-pane label="所选素材信息" type="card">
             <div id="currentSourceHtml2" style="float:left;position: absolute; left:-5500px;" />
-            <div id="imgContent" style="float:left;position: absolute; left:-6000px;" />
+            <div id="imgContent" />
             <div v-show="sourceDivVisiable" v-if="currentSource" id="sourceDiv">
               <el-row>
                 <el-col :span="12">
                   <el-form-item label="名称">
-                    <el-input id="currentSourceName" v-model="currentSource.name" @change="sourceChange" @input="inputChange()" />
+                    <el-input
+                      id="currentSourceName"
+                      v-model="currentSource.name"
+                      @change="sourceChange"
+                      @input="inputChange()"
+                    />
                   </el-form-item>
                 </el-col>
                 <el-col :span="12">
@@ -432,7 +443,12 @@
                 <el-row>
                   <el-col :span="24">
                     <el-form-item label="URL">
-                      <el-input id="currentSourceUrl" v-model="currentSource.url" @change="sourceChange" @input="inputChange()" />
+                      <el-input
+                        id="currentSourceUrl"
+                        v-model="currentSource.url"
+                        @change="sourceChange"
+                        @input="inputChange()"
+                      />
                       <span style="color:red">以https开头</span>
                     </el-form-item>
                   </el-col>
@@ -478,10 +494,9 @@
                       <div v-if="currentSource && currentSource.backgroundColor">
                         <section>
                           <div
-                            class="color_con"
                             :style="{background:currentSource.backgroundColor}"
                             @click="handleShowColor"
-                          >选择颜色
+                          >确定
                           </div>
                           <div v-show="colorShow">
                             <sketch-picker v-model="currentSource.backgroundColor" @input="updateValue" />
@@ -490,7 +505,7 @@
                       </div>
                       <div v-else>
                         <section>
-                          <div class="color_con" :style="{background:color}" @click="handleShowColor">选择颜色</div>
+                          <div class="color_con" :style="{background:color}" @click="handleShowColor">确定</div>
                           <div v-show="colorShow">
                             <sketch-picker v-model="color" @input="updateValue" />
                           </div>
@@ -514,7 +529,12 @@
                 <div v-show="textDivVisiableV2">
                   <el-row>
                     <el-col :span="12">
-                      <el-form-item id="currentSoureCenterV2" label="垂直居中" @change="sourceChange" @input="inputChange()">
+                      <el-form-item
+                        id="currentSoureCenterV2"
+                        label="垂直居中"
+                        @change="sourceChange"
+                        @input="inputChange()"
+                      >
                         <el-select v-model="currentSource.fixCenter" placeholder="请选择">
                           <el-option :value="true" label="是" />
                           <el-option :value="false" label="否" />
@@ -539,7 +559,13 @@
                 <el-row>
                   <el-form-item label="内容" prop="html">
                     <p><span style="color:red">提示: 按Shift+回车换行，按回车换页</span></p>
-                    <editor id="currentSourceHtml" v-model="currentSource.html" :init="editorInit" @change="sourceChange" @input="editorInputChange()" />
+                    <editor
+                      id="currentSourceHtml"
+                      v-model="currentSource.html"
+                      :init="editorInit"
+                      @change="sourceChange"
+                      @input="editorInputChange()"
+                    />
                     <!--                    <el-button type="primary" size="mini" @click="handleHtml()">确定修改文本内容</el-button>-->
                   </el-form-item>
                 </el-row>
@@ -702,7 +728,7 @@ html{
   height:100%;
 }
 #package{
-  width: 1000px;
+  width: 90%;
   height: 800px;
   overflow: auto;
   border:1px red solid;
@@ -1280,7 +1306,7 @@ export default {
       this.$router.push({ path: '/screen/program' })
     },
     handleConfirm() {
-      // this.program.layers[0].sources = this.playSourceList
+      this.editorInputChange()
       console.log(this.program)
       this.$refs['program'].validate(valid => {
         if (valid) {
@@ -1759,26 +1785,25 @@ export default {
       } else {
         bgColor = this.color
       }
-      // document.getElementById('currentSourceHtml2').innerHTML = '<div id="imgDiv" background: ' + bgColor + '">' + document.getElementById('currentSourceHtml').value + '</div>'
-      // document.getElementById('currentSourceHtml2').innerHTML = '<div id="imgDiv" background: ' + bgColor + '">' + '12345678' + '</div>'
-      // const htmlDom = document.getElementById('imgDiv')
+      document.getElementById('currentSourceHtml2').innerHTML = '<div id="imgDiv" style="background:' + bgColor + '">' + this.currentSource.html + '</div>'
+      const htmlDom = document.getElementById('imgDiv')
 
-      const imgDiv = document.createElement('div')
-      imgDiv.id = Math.random()
-      imgDiv.style.background = bgColor
-      imgDiv.innerHTML = this.currentSource.html
-      document.getElementById('currentSourceHtml2').innerHTML = imgDiv.innerHTML
-      const htmlDom = document.getElementById('currentSourceHtml2')
+      // const imgDiv = document.createElement('div')
+      // imgDiv.id = Math.random()
+      // imgDiv.style.background = bgColor
+      // imgDiv.innerHTML = this.currentSource.html
+      // document.getElementById('currentSourceHtml2').innerHTML = imgDiv.innerHTML
+      // const htmlDom = document.getElementById('currentSourceHtml2')
 
-      console.log('打印', htmlDom)
+      console.log('打印', bgColor, htmlDom)
       html2canvas(htmlDom, {
         // allowTaint: false, // 允许污染
         // taintTest: true, // 在渲染前测试图片(没整明白有啥用)
-        useCORS: true // 使用跨域(当allowTaint为true时这段代码没什么用)
+        useCORS: true, // 使用跨域(当allowTaint为true时这段代码没什么用)
         // height: htmlDom.offsetHeight,
         // width: htmlDom.offsetWidth,
-        // scrollY: 0,
-        // scrollX: 0 // 解决白边
+        scrollY: 0,
+        scrollX: -23 // 解决白边
 
         // backgroundColor:null,
       }).then(canvas => {
@@ -2634,7 +2659,7 @@ export default {
     updateValue(val) {
       this.color = 'rgba(' + val.rgba.r + ',' + val.rgba.g + ',' + val.rgba.b + ',' + val.rgba.a + ')'
       this.currentSource.backgroundColor = this.color
-      // this.editorInputChange()
+      this.editorInputChange()
     },
     handleHtml() {
       this.editorInputChange()
